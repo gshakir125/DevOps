4d18981eeadd74de31b6d5289d538a2ab9387aff
Abdullah Farooq <abdullah.gb@hotmail.com>
2019-06-21 16:24:38 +0500

    add: drop script

---
+++ drop.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+dropFile=$1
+dropDir=$2
+devOpsDir="DevOps"
+
+unzip /drop/$dropFile.zip -d /$devOpsDir/nginx/static/$dropDir
+
+command cd $devOpsDir
+if [ -d .git ]; then
+    docker-compose -f docker-compose.yml -f docker-compose.qa.yml restart nginx
+fi
+exit 0
