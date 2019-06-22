7838fb5b2ae7156660f945f777fe44c211d47fa0
Abdullah Farooq <abdullah.gb@hotmail.com>
2019-06-22 06:00:47 +0500

    fix: edit update xrdp link

--- installLubuntu.sh
+++ installLubuntu.sh
@@ -7,6 +7,8 @@ lxsession -e LXDE -s Lubuntu
 # nano /etc/xrdp/startwm.sh
 # last line lxsession -s Lubuntu -e LXDE
 sudo service xrdp restart
+# For Update xdrp 
+# https://github.com/neutrinolabs/xrdp/wiki/Building-on-Debian-8
 sudo chown "$USER":"$USER" /home/"$USER"/.docker -R
 sudo chmod g+rwx "/home/$USER/.docker" -R
 
