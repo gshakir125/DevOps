70c00c3236ab581340ebf0cdc9964faaf2511430
Abdullah Farooq <abdullah.gb@hotmail.com>
2019-06-22 14:52:34 +0500

    en: install linux script

--- installLubuntu.sh
+++ installLubuntu.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
 set -e
-apt update && apt -y dist-upgrade
+sudo apt update && sudo apt -y dist-upgrade
 sudo apt-get install lubuntu-desktop
-sudo apt-get install xrdp
-lxsession -e LXDE -s Lubuntu
+sudo apt update && sudo apt -y dist-upgrade
+
+# https://github.com/neutrinolabs/xrdp/wiki/Building-on-Debian-8
 # nano /etc/xrdp/startwm.sh
 # last line lxsession -s Lubuntu -e LXDE
+sudo systemctl enable xrdp
 sudo service xrdp restart
-# For Update xdrp 
-# https://github.com/neutrinolabs/xrdp/wiki/Building-on-Debian-8
+
+# Themeing
+# https://www.deviantart.com/satya164/art/elementary-Dark-GTK3-Theme-244257862
+
+# Docker 
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
+# If still Permission denied
+sudo chmod 666 /var/run/docker.sock
+
+mkdir .docker
 sudo chown "$USER":"$USER" /home/"$USER"/.docker -R
 sudo chmod g+rwx "/home/$USER/.docker" -R
 
